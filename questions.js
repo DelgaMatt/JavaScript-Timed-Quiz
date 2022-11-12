5606e2358d508f41ca02ed9167ea779210e06ad4
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2022-11-11 18:19:07 -0800

    added questions.js and included content. began working on major functions

---
+++ questions.js
@@ -0,0 +1,37 @@
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<script>", "<javascript>", "<js>", "<scripting>"],
+        answer: "<script>"
+    },
+
+    {
+        title: "Which of the following function of Array object joins all elements of an array into a string?",
+        choices: ["concat()", "join()", "pop()", "map()"],
+        answer: "concat()"
+    },
+
+    {
+        title: "Which of the following type of variable is visible only within a function where it is defined?",
+        choices: ["global variable", "local variable", "both of the above", "none of the above"],
+        answer: "local variable"
+    },
+
+    {
+        title: "What is 'this' keyword in JavaScript?",
+        choices: ["refers to a function", "refers to your current existential crisis", "refers to the first variable in a function", "refers to the object from where it was called"],
+        answer: "refers to the object from where it was called"
+    },
+
+    {
+        title: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+        choices: ["if (i <> 5)", "if (i != 5)", "if i =! 5 then", "if i = 5"],
+        answer: "if (i != 5)"
+    },
+
+    {
+        title: "How do you call a function named 'myFunction?'",
+        choices: ["call my function myFunction()", "call function(myFunction)", "myFunction()", "Come here my precious function!"],
+        answer: "myFunction()"
+    }
+];
